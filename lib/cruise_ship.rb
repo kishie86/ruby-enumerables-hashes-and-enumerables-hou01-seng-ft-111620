f314d65d03db53f5cde092704f5fc4b8940bab24
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-28 14:19:40 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = " "
   passengers.each do |suites, name|
-    if suite == :suite_a && name.start_with?("A")
+    if suites == :suite_a && name.start_with?("A")
       winner = name
 end
 end
